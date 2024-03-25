b777650c59113768aef6a4ecb30d660f0f5a1a23
Jolomi-Tosanwumi <tjolomi@gmail.com>
2024-03-25 06:03:32 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='tscluster',
-   version='0.1.0',
+   version='1.0.0',
    description='A useful package for temporal clustering',
    license="MIT",
    long_description=long_description,
