8fce3b9d341554e4e420f3c230344d6b186a1d57
Jolomi-Tosanwumi <tjolomi@gmail.com>
2024-03-24 22:53:00 -0400

    uncommented lines for testing opttscluster in test_main.py

--- test_main.py
+++ test_main.py
@@ -1,27 +1,6 @@
 import numpy as np
 
-# from tscluster.opttscluster import OptTSCluster
-
-# X_mini = np.load("./sythetic_data.npy")
-
-# kc_opt_model = OptTSCluster(3, random_state=42, use_full_constraints=1, warm_start=1, 
-#                           scheme='z1c1', n_allow_assignment_change=None,
-#                              use_MILP_centroid=False, is_tight_constraints=False, is_Z_positive=True,
-#                              init_with_prev=False, use_sum_distance=False)
-# kc_opt_model.fit(X_mini)
-
-# print(f"shape of labels_ is {kc_opt_model.labels_.shape}")
-# print("head of labels_ is:")
-# print(kc_opt_model.labels_[:5, :])
-
-# print(f"shape of cluster_centers_ is {kc_opt_model.cluster_centers_.shape}")
-# print("head of cluster_centers_ is:")
-
-# if len(kc_opt_model.cluster_centers_.shape) == 3:
-#     print(kc_opt_model.cluster_centers_[:5, :, :])
-# else:
-#     print(kc_opt_model.cluster_centers_)
-
+from tscluster.opttscluster import OptTSCluster
 
 from tslearn.generators import random_walks
 from tslearn.clustering import TimeSeriesKMeans
@@ -29,6 +8,26 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from tscluster.tskmeans import TSKmeans, TSGlobalKmeans
 from tscluster.preprocessing import utils as preprocess_utils, TSStandardScaler, TSMinMaxScaler
+
+X_mini = np.load("./sythetic_data.npy")
+
+kc_opt_model = OptTSCluster(3, random_state=42, use_full_constraints=1, warm_start=1, 
+                          scheme='z1c1', n_allow_assignment_change=None,
+                             use_MILP_centroid=False, is_tight_constraints=False, is_Z_positive=True,
+                             init_with_prev=False, use_sum_distance=False)
+kc_opt_model.fit(X_mini, verbose=False)
+
+print(f"shape of labels_ is {kc_opt_model.labels_.shape}")
+print("head of labels_ is:")
+print(kc_opt_model.labels_[:5, :])
+
+print(f"shape of cluster_centers_ is {kc_opt_model.cluster_centers_.shape}")
+print("head of cluster_centers_ is:")
+
+if len(kc_opt_model.cluster_centers_.shape) == 3:
+    print(kc_opt_model.cluster_centers_[:5, :, :])
+else:
+    print(kc_opt_model.cluster_centers_)
 
 X = random_walks(n_ts=50, sz=32, d=2, random_state=42)
 Xt = preprocess_utils.NTF_to_TNF(X)
---
+++ tscluster/metrics/metrics.py
@@ -0,0 +1,2 @@
+def sse_score(X, cluster_centers, labels):
+    pass 
