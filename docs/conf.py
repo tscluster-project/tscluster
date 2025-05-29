8ef59713470e2deb206c88b8509e5adc9a4fcb9f
Joe Liang <50597009+joeliang0520@users.noreply.github.com>
2025-05-29 14:29:37 +0800

    update documentation for tscluster

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 project = 'tscluster'
 copyright = '2024, tscluster'
-author = 'Jolomi Tosanwumi'
+author = 'Jolomi Tosanwumi, Jiazhou Liang'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
